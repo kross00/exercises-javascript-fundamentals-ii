d63ab20a75d6d6ffb7ab6a0b316e07862d948b61
Kendahl Ross <kendahlross@Kendahls-MacBook-Air.local>
2020-01-24 13:53:04 -0800

    LET'S GO: formatSeconds

--- exercises/formatSeconds/formatSeconds.js
+++ exercises/formatSeconds/formatSeconds.js
@@ -17,33 +17,30 @@
  */
 
 function formatSeconds(num) {
-  // This is your job. :)
-
-  // Remember, if the code is stumping you, take a step back and
-  // make sure you can do it by hand.
-}
-
-if (require.main === module) {
-  console.log('Running sanity checks for formatSeconds:');
-
-  /*
-  Add your own test cases here! These four aren't enough.
-
-  Notice that we're looking at "edge cases": the boundary where the
-  parts "flip over", and also the values on either side of that boundary.
-
-  This is where the code is most likely to go wrong.
-  */
-
-  console.log(formatSeconds(0) === '0s');
-  console.log(formatSeconds(1) === '1s');
-
-  console.log(formatSeconds(55) === '55s');
-  console.log(formatSeconds(60) === '1m 0s');
-  console.log(formatSeconds(65) === '1m 5s');
-
-  console.log(formatSeconds(3600) === '1h 0m 0s');
-  console.log(formatSeconds(3615) === '1h 0m 15s');
-}
-
-module.exports = formatSeconds;
+  let lessThanAMinute = num<60;
+  let moreThanAMinute = num>60;
+  let minute = sixtySeconds;
+  
+  if(num<60){
+      return lessThanAMinute;
+  
+  }else{
+      if(num>60)
+  return moreThanAMinute
+  }
+  
+  if(num=lessThanAMinute){
+      return(num + 's')
+  
+      
+      }
+  }
+  
+  
+  if (require.main === module) {
+      console.log('Running sanity checks for selectLessThan:');
+    console.log(formatSeconds(45)); 
+  
+    }
+    
+    module.exports = formatSeconds;
